70c56991d24766e8e20d8986460c51f6fd4d3036
Phoenixthrush UwU <mr.cetto@phoenixthrush.com>
2023-10-29 16:50:59 +0100

    Cleaning things up...

---
+++ README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d71b9a310b41f127feb800beeca2bbc46a2018bbd97ffc0d4c83d529a3503d60
+size 162
---
+++ dev.sh
@@ -0,0 +1,30 @@
+mkdir -p raw
+mkdir -p output
+
+rm -f raw/*.txt
+rm -f output/*.txt
+
+cd raw
+count=0
+
+while read -r url; do
+  echo "Downloading: [$count] $url"
+  filename="${count}.txt"
+  curl -sSL "$url" >> "$filename".txt
+  ((count++))
+done < ../sources.txt
+
+echo -e "\nProcessing files."
+cp ../phoenixthrush.txt .
+cat * > ../output/raw.txt
+
+cd ../output
+du -hs raw.txt
+
+cat raw.txt | sort --parallel --ignore-leading-blanks --ignore-nonprinting --ignore-case > sorted.txt
+du -hs sorted.txt
+
+uniq --ignore-case --unique sorted.txt unique.txt
+du -hs unique.txt
+
+cp unique.txt ..
